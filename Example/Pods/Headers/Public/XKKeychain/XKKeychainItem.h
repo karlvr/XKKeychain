3873456dcdb040d1c60145a78f19443a30d76dab
Karl von Randow <karl@xk72.com>
2014-10-24 22:34:10 +1300

    Add Pods for tests

---
+++ Example/Pods/Headers/Public/XKKeychain/XKKeychainItem.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/XKKeychainItem.h
---
+++ Example/Pods/Target Support Files/Pods-XKKeychain-XKKeychain/Pods-XKKeychain-XKKeychain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XKKeychain_XKKeychain : NSObject
+@end
+@implementation PodsDummy_Pods_XKKeychain_XKKeychain
+@end
